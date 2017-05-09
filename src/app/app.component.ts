814585075a6059d21deffad82d4aa10e7880685c
Xu <590939@bah.com>
2017-05-08 23:08:52 -0400

    First commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component }          from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  template: `
+    <img id="logo" style="text-align: center;" src="../../img/logo.png" alt="FIU AlertSandBox" />
+    <h1>{{title}}</h1>
+    <nav>
+      <a routerLink="/dashboard" routerLinkActive="active">Dashboard</a>
+      <!--<a routerLink="/heroes" routerLinkActive="active">Heroes</a>
+      <a routerLink="/alert" routerLinkActive="active">Alert</a>-->
+      <a routerLink="/report" routerLinkActive="active">Report</a>
+      <a routerLink="/configuration" routerLinkActive="active">Configuration</a>
+      <a routerLink="/admin" routerLinkActive="active">Admin</a>
+    </nav>
+    <router-outlet></router-outlet>
+  `,
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'FIU AlertSandBox';
+}
