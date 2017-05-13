af55d916e10fbef70e75e43b33a28e191ac231a1
Xu <590939@bah.com>
2017-05-13 16:09:35 -0400

    Second commit with bookstore

---
+++ src/js/app.js
@@ -0,0 +1,87 @@
+/**
+ * Project the entrance of the module，depend on ui.router ui.grid
+ * And our custom two modules，BookListModule和BookDetailModule
+ */
+
+
+
+var bookStoreApp = angular.module('bookStoreApp', ['ui.router', 'ui.grid','ui.grid.pagination', 'BookListModule', 'BookDetailModule', 'loginModule', 'addBookModule']);
+
+/**
+ * Because the whole application will deal with the routing，So here are the $state and $stateParams The two objects put into $rootScope，Convenient place other references and injection.
+ * The run method here will run only one time when angular start running 
+ * @param  {[type]} $rootScope
+ * @param  {[type]} $state
+ * @param  {[type]} $stateParams
+ * @return {[type]}
+ */
+
+
+
+//Attention :$state and $stateParams comeing from ui-route
+//https://github.com/angular-ui/ui-router/wiki/URL-Routing
+//$stateParams status parapeters，$stateParams can provide controller or service the separate parts of the url.
+
+
+bookStoreApp.run(function($rootScope, $state, $stateParams){
+	$rootScope.$state = $state;
+	$rootScope.$stateParams = $stateParams;
+});
+
+
+/**
+ * Confihure router
+ * Note that there is ui-router，Instead of ng native routing.
+ * The ng native routing does not support nested view.
+ * @param  {[type]} $stateProvider
+ * @param  {[type]} $urlRouterProvider
+ * @return {[type]}
+ */
+bookStoreApp.config(function($stateProvider, $urlRouterProvider) {
+    // If there is no match to other routing, the direct return to the home page
+    $urlRouterProvider.otherwise('/index');
+    // Routing and configuration
+    $stateProvider
+        .state('index', {
+            // If it is first page
+            url: '/index',
+            views: {
+                '': {
+                // Is the home page if it is empty
+                    templateUrl: 'tpls/home.html'
+                },
+                'main@index': {
+                    // If you have embedded routines by is the registration page
+                    templateUrl: 'tpls/login_form.html'
+                }
+            }
+        })
+        .state('booklist', {
+            // Book List
+            url: '/{bookType:[0-9]{1,4}}',
+            views: { //Pay attention to the writing here, When a page with multiple UI - view to naming and view template when loading
+                '': {
+                // Home page is booklist
+                    templateUrl: 'tpls/booklist.html'
+                },
+                'booktype@booklist': {
+                    // booktype Routing for the booktype template
+                    templateUrl: 'tpls/book_type.html'
+                },
+                'bookgrid@booklist': {
+                    // bookgrid Routing for the bookgrid template
+                    templateUrl: 'tpls/bookgrid.html'
+                }
+            }
+        })
+        .state('addbook', {
+            // add books
+            url: '/addbook',
+            templateUrl: 'tpls/addbook_form.html'
+        })
+        .state('bookdetail', {
+            // Details of the book
+            url: '/bookdetail/:bookId', //Note here the parameters in the routing
+            templateUrl: 'tpls/book_details.html'
+        })
+});
---
+++ src/js/controllers.js
@@ -0,0 +1,185 @@
+/**
+ * Here is the registration module
+ */
+
+var loginModule = angular.module('loginModule', []);
+loginModule.controller('loginCtrl', ['$scope', function($scope){
+    $scope.userInfo = {
+        email: 'sample@bmo.com',
+        password: '12345679'
+    };
+    $scope.setFormData = function(){
+        $scope.userInfo = {
+            email: '',
+            password: ''
+        };
+    };
+}]);
+
+
+/**
+ * Here is a list of books module
+ * @type {[type]}
+ */
+
+var BookListModule = angular.module('BookListModule', []);
+BookListModule.controller('BookListCtrl', ['$scope', '$http', '$state', '$stateParams',
+ function($scope, $http, $state, $stateParams){
+
+//Here you can according to different routing passed bookType parameters on loading of data
+/**
+ * [getPagedDataAsync For asynchronous data]
+ * @param  {[type]} pageSize   [The number of data in the each page]
+ * @param  {[type]} page       [The serial number of the current page]
+ * @param  {[type]} searchText [Search-based Keyword]
+ */
+
+  $scope.getPagedDataAsync = function(page, pageSize, searchText) {
+      setTimeout(function() {
+          var data;
+          if (searchText) {
+            // Search Text
+              var ft = searchText.toLowerCase();
+              // get json file
+              $http.get('data/books' + $stateParams.bookType + '.json')
+                  .then(function(response) {
+
+                    // if successful
+                      data = response.data.filter(function(item) {
+                        // If found, returns true
+                          return JSON.stringify(item).toLowerCase().indexOf(ft) != -1;
+                      });
+                      $scope.setPagingData(data, page, pageSize);
+                  }, function(){
+                    console.log('http request failed' + response.status);
+                  });
+          } else {
+              $http.get('data/books' + $stateParams.bookType + '.json')
+                  .then(function(response) {
+                      $scope.setPagingData(response.data, page, pageSize);
+                  }, function(){
+                    console.log('http request failed' + response.status);
+                  });
+          }
+      }, 100);
+  };
+
+
+  var paginationOptions = {
+     pageNumber: 1,
+     pageSize: 5,
+     sort: null
+   };
+
+/**
+ * [setPagingData description]
+ * @param {[type]} data     [The root of the trouble to get the data]
+ * @param {[type]} page     [The serial number of the current page]
+ * @param {[type]} pageSize [The number of each data]
+ */
+  $scope.setPagingData = function(data, page, pageSize) {
+    // Total number of data for the current page, The article according to the total number of the current page, Assigned to ui-grid's data attribute
+    var firstRow = (page - 1) * pageSize;
+    $scope.gridOptions.data = data.slice(firstRow, firstRow + pageSize);
+      // Get all the data on the number of article from the background
+     $scope.gridOptions.totalItems = data.length;
+  };
+
+//Get the current page and current size from the UI - gird
+  $scope.getPagedDataAsync(paginationOptions.pageNumber,paginationOptions.pageSize);
+	
+//With the backgend dataui-grid,Fill the form
+     $scope.gridOptions = {
+        //Note that the value of the field to guarantee a json fields to display
+        columnDefs: [{ field: 'index', 
+                       displayName: 'Serial No', 
+                       enableColumnMenu: false,// Whether or not display the column head menu button
+                       enableHiding: false,
+                       suppressRemoveSort: true,
+                       enableCellEdit: false // Whether or not editable
+                     },
+                     { field: "name",
+                       displayName: 'Book Name'
+                 	 },
+                    { field: "author",
+                      displayName: 'author'
+                	 },
+                    { field: "pubTime",
+                      displayName: 'Pub date'
+                	 },
+                    { field: "price",
+                      displayName: 'Price',
+                      cellFilter: 'currency:"$"'
+                	 },	 
+                    { field: "bookId",
+                      displayName: 'Operation',
+                      cellTemplate: '<div><a ui-sref="bookdetail({bookId:grid.getCellValue(row, col)})">Details</a></div>'
+                    }
+                    ],
+                    
+        //-------- page attribute ----------------
+        enablePagination: true, //Whether the page, the default is true
+        enablePaginationControls: true, //Use the default to the bottom of the page
+        paginationPageSizes: [5, 10, 15], //Each page shows the number of options
+        paginationPageSize: 5, //Shows the number of each page
+        paginationCurrentPage:1, //current page
+        //paginationTemplate:"<div></div>", //The custom code at the bottom of the page
+        totalItems : 0, // Total
+        useExternalPagination: true,//Whether to use pagination button
+        useExternalSorting: false, //Whether to use a self-define rule
+        //---------------api---------------------
+        // onRegisterApi: function(gridApi) {
+        //     $scope.gridApi = gridApi;
+        //     //Paging button events
+        //     gridApi.pagination.on.paginationChanged($scope,function(newPage, pageSize) {
+        //           if(getPage) { 
+        //               getPage(newPage, pageSize); 
+        //            }
+        //     });
+        // }
+    };
+
+
+}]);
+
+
+/**
+ * Books of module for details
+ * @type {[type]}
+ */
+var BookDetailModule = angular.module('BookDetailModule', []);
+BookDetailModule.controller('BookDetailCtrl', ['$scope', '$http', '$state', '$stateParams',
+ function($scope, $http, $state, $stateParams){
+
+var bookId = $stateParams.bookId;
+
+$scope.getDetailDataAsync = function(bookId) {
+    setTimeout(function() {
+        $http.get('data/books0.json')
+            .then(function(response) {
+               $scope.details = response.data.filter(function(item){
+                     return item.bookId == bookId;
+                })[0];
+               //Returns an array, it is only an object [0] Take out the object
+            }, function(){
+              console.log('http request failed' + response.status);
+            });
+    }, 100);
+};
+
+$scope.getDetailDataAsync(bookId);
+
+}]);
+
+
+
+/**
+ * Add books module
+ */
+
+ var addBookModule = angular.module('addBookModule', []);
+ addBookModule.controller('addBookCtrl', ['$scope', function($scope){
+     $scope.emptyData = function() {
+        $scope.book = {};
+     }
+ }]);
